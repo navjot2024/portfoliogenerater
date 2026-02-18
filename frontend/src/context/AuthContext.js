a2584b410eccaa2d771726f774dde694e222a1d0
Navjot Kaur <navjotkaur24@navgurukul.org>
2026-02-18 13:52:45 +0530

    Done

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,4 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-import axios from 'axios';
 import api from '../services/api';
 
 const AuthContext = createContext();
